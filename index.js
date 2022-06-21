c271338b09228fad09a3fb0d28b232c9aad1e26d
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-06-21 23:43:03 +0800

    update functional, next testing for the large data

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
-const request = require('request');
-const fs = require('fs')
-const kecamatans = [];
+const request = require('request'); // request
+const buffer = require('buffer'); // request
+const fs = require('fs') // filesystem
+const kecamatans = []; // array for kecamatans
 
-const get_location = async () => {
-  for (let index = 5000001; index <= 5000040; index++) {
+
+// for testing the structure example
+const getStructure = async () => {
+  const url = `https://www.olx.co.id/api/locations/5000001/path`
+  const response = await fetch(url);
+  const data = await response.json();
+  done = data;
+  // console.log(done['data'][0]);
+}
+
+// to iterate through the kecamatans data
+const iterateObject = async (kecamatans) => {
+  const kecamatan = [];
+  kecamatans.forEach(element => {
+    let object = {
+      "nama_kecamatan": element.name,
+      "tipe": element.type,
+      "longitude": element.longitude,
+      "latitude": element.latitude,
+      "id_kota": element.parentId,
+      "nama_kota": element['addressComponents'][2]['name']
+    };
+    kecamatan.push(object);
+    // data.push(kecamatan);
+    // data = JSON.strigify(data, null, 2);
+  });
+  let data = JSON.stringify(kecamatan);
+  // console.log(data);
+  writeData(data);
+}
+
+// get the location for all and assign the kecamatan ('NEIGHBOURHOOD')
+const getLocation = async () => {
+  for (let index = 5000001; index <= 5000005; index++) {
     const url = `https://www.olx.co.id/api/locations/${index}/path`
     const response = await fetch(url);
     const data = await response.json();
-    kecamatans.push(data);
+    kecamatans.push(data['data'][0]);
   }
-
-  for (let index = 0; index < 4; index++) {
-    console.log(kecamatans[index]['data'][0].name);
-    console.log(kecamatans[index]['data'][0].type);
-    console.log(kecamatans[index]['data'][0].longitude);
-    console.log(kecamatans[index]['data'][0].latitude);
-  }
+  iterateObject(kecamatans);
 }
 
-get_location();
+// for writing the data
+const writeData = async (data) => {
+  // get the array 
+  const text = new Uint8Array(Buffer.from(data));
+  const fileName = "./kecamatan.json";
+  fs.writeFile(fileName, text, (error)  => {
+    if (error) throw error;
+    console.log("Data Kecamatan Tersimpan !");
+  });
+}
+
+// getStructure(); // to get the object example
+getLocation();
