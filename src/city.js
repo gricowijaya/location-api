d88c8c0e24855f61a447a357e1b84191dad3c5db
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-06-23 21:26:42 +0800

    rename kota.js to city.js

--- src/city.js
+++ src/city.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch')
 const fs = require('fs') // filesystem
-const kecamatans = []; // array for kecamatans
+const cities = []; // array for citys
 
 
 // for testing the structure example
 const getStructure = async () => {
-  // latest kecamatans
+  // latest cities
   const url = `https://www.olx.co.id/api/locations/5007094/path`
   const response = await fetch(url);
   const data = await response.json();
@@ -13,50 +13,56 @@ const getStructure = async () => {
   console.log(done['data'][0]);
 }
 
-// to iterate through the kecamatans data
-const iterateObject = async (kecamatans) => {
-  const kecamatan = [];
-  kecamatans.forEach(element => {
+// to iterate through the cities data
+const iterateObject = async (cities) => {
+  const city = [];
+  cities.forEach(element => {
     let object = {
-      "id_kecamatan": element.id,
-      "nama_kecamatan": element.name,
-      "tipe": element.type,
+      "id": element.id,
+      "name": element.name,
+      "type": element.type,
+      "description": `Selamat Datang di Open Chat Wilayah ${element.name}` ,
       "longitude": element.longitude,
       "latitude": element.latitude,
-      "id_kota": element.parentId,
-      "nama_kota": element['addressComponents'][2]['name']
+      "city_id": element.parentId,
+      "city_name": element['addressComponents'][2]['name'], 
+      "image" : `testing_image`
     };
-    kecamatan.push(object);
-    // data.push(kecamatan);
+    city.push(object);
+    // data.push(city);
     // data = JSON.strigify(data, null, 2);
   });
-  let data = JSON.stringify(kecamatan);
+  let data = JSON.stringify(city);
   // console.log(data);
   writeData(data);
 }
 
-// get the location for all and assign the kecamatan ('NEIGHBOURHOOD')
+// get the location for all and assign the city ('CITY')
 const getLocation = async () => {
   // loop until the latest data
-  for (let index = 5000001; index <= 5007094; index++) {
+  // for (let index = 5000001; index <= 5007094; index++) {
+  for (let index = 5000001; index <= 5000001; index++) {
     const url = `https://www.olx.co.id/api/locations/${index}/path`
     const response = await fetch(url);
     const data = await response.json();
-    kecamatans.push(data['data'][0]);
+    cities.push(data['data'][0]);
   }
-  iterateObject(kecamatans);
+  iterateObject(cities);
 }
 
 // for writing the data
 const writeData = async (data) => {
   // get the array 
   const text = new Uint8Array(Buffer.from(data));
-  const fileName = "./kecamatan.json";
+  const fileName = "./resource/city.json";
   fs.writeFile(fileName, text, (error)  => {
     if (error) throw error;
-    console.log("Data Kecamatan Tersimpan !");
+    console.log(`City data is saved at ${fileName} !`);
   });
 }
 
 // getStructure(); // to get the object example
-getLocation();
+// getLocation();
+
+module.exports =  {writeData, getLocation, getStructure, iterateObject}
+
