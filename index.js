0e63064cac3789f6019724bd2e093834f9a6d65d
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-13 13:09:01 +0800

    update the index js using express

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const command = require('./src/js/command');
+// const command = require('./src/js/command');
 
 // command.run.generateLocation();
 // command.run.generateAvatarForLocation();
-command.run.generateLocation();
+// command.run.generateLocation();
+
+require('dotenv').config(); // to read the env file
+const express = require("express");
+const app = express();
+const routes = require('./src/routes');
+
+// app.use('/api/v1', routes);
+
+app.use('/api/v1/', routes);
+
+// home endpoint
+app.get("/", (req, res) => { 
+  return res.json({
+    status: 200,
+    data: "Bali" 
+  })
+});
+
+app.listen(process.env.HTTP_PORT, () =>{
+  console.log(`success! listening on port ${process.env.HTTP_PORT}`);
+});
