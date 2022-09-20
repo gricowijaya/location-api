eff6f326ab3e6aa92ec7cbe8468458ef4c507cd6
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-09-20 19:00:03 +0800

    add docker file

--- src/controller/index.js
+++
@@ -1,6 +0,0 @@
-const location = require('./location')
-const profile = require('./profile')
-const neighbourhood = require('./neighbourhood')
-const city = require('./city')
-
-module.exports = {location, profile, neighbourhood, city}
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const neighbourhood = require('../routes/neighbourhood');
-
-router.use('/neighbourhood', neighbourhood);
-
-module.exports = router;
-
